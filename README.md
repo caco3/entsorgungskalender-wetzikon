46e32ef58d15e9c2a08c0690e5673bf3a88f6420
George Ruinelli <caco3@ruinelli.ch>
2022-06-11 17:52:27 +0200

    Drop the VCALENDAR header/footer, it is only used per file and not per VEVENT. update readme and example database

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 This tool got created on the first [Smart City-Hackathon of Wetzikon, Switzerland](https://smart-city-wetzikon.ch/hackathon-2/).
 
 Public Data: https://wetzikon.ch/verwaltung/entsorgung resp. https://m.wetzikon.ch/index.php?apid=7215088
+A nice events overview is available on https://wetzikon.ch/verwaltung/entsorgung/02_GzD_Entsorgungskalender_2022_A3_Stadt_Wetzikon.pdf/at_download/file
 
 provided tools:
  * `get-apids.py`: This tool fetches the districts (Kreis) and the categories. The fetched data gets stored in `data/apids.json`.
  * `get-events.py`: This tool fetches all events based on `data/apids.json` and stores the evenets in `data/database.json`.
+
+The categories 'Sonderabfall', 'Papier' and 'Schleifservice' are common to all districts. However the website lists them again (with different apids) on all districts. For simplicity we keep it that way, meaning one always has to select a district.
 
 Example data is provides in `data/*-2022.json`. This data got grabbed on 11.06.2022.
--- grab-events.py
+++ grab-events.py
@@ -27,6 +27,7 @@ database = json.load(f)
 for district in database:
     logging.info("Fetching events in district %r..." % district)
     for category in database[district]["categories"]:
+        logging.info("  Category: %r..." % category)
         apid = database[district]["categories"][category]["apid"]
         page = requests.get(domain + "/" + pathAndQuery + str(apid)).text
         soup = BeautifulSoup(page, 'html.parser')
@@ -40,9 +41,11 @@ for district in database:
                 logging.debug("Event in %r/%r: %r" % (district, category, date))
 
                 ics = requests.get(domain + "/" + urlIcs).text
-                database[district]["categories"][category]["events"].append({ "date": date, "vcal": ics})
-
-
+                vevent = "BEGIN:VEVENT" + ics.split("BEGIN:VEVENT")[1].split("END:VEVENT")[0] + "END:VEVENT" # Drop the VCALENDAR header/footer, it is only used per file and not per VEVENT
+                database[district]["categories"][category]["events"].append({ "date": date, "vevent": vevent})
+            # break # Testing
+        # break # Testing
+    # break # Testing
 
 # pprint(database)
 
